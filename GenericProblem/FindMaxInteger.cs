391bb9e3bfd59db6a54eab3a99297fc3420c1dce
singhtushar689 <Singhtushar689@gmail.com>
2023-02-25 22:32:37 +0530

    [Tushar] add: Find Max Integer

---
+++ GenericProblem/FindMaxInteger.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericProblem
+{
+    public class FindMaxInteger
+    {
+        public void MaxValue(int first, int second, int third)
+        {
+            if (first.CompareTo(second) >= 0 && first.CompareTo(third) >= 0)
+                Console.WriteLine($"{first} is greater");
+            if (second.CompareTo(third) >= 0 && second.CompareTo(third) >= 0)
+                Console.WriteLine($"{second} is greater");
+            if (third.CompareTo(first) >= 0 && third.CompareTo(second) >= 0)
+                Console.WriteLine($"{third} is greater");
+        }
+    }
+}
---
+++ GenericProblem/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericProblem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            FindMaxInteger max = new FindMaxInteger();
+            max.MaxValue(10, 35, 31);          
+            Console.ReadLine();
+            FindMaxGeneric<int> maxGeneric = new FindMaxGeneric<int>();
+            maxGeneric.Max(23, 34, 21);
+
+        }
+    }
+}
